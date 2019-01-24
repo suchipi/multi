a885a52a0c1c65f7322b4cd3ffdc282ec61cd63a
Lily Scott <me@suchipi.com>
2019-01-23 19:12:51 -0800

    Initial commit

---
+++ babel.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  presets: ["@babel/preset-typescript"],
+  plugins: [
+    "@babel/plugin-proposal-object-rest-spread",
+    "babel-plugin-transform-es2015-modules-commonjs"
+  ]
+};
